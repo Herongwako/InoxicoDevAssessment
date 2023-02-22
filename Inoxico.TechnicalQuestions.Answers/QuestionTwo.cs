b0df6716486ab58b34a0b3d03beeecf36a7ace49
Herold-willcom <heroldm@willcom.co.za>
2023-02-22 22:38:35 +0200

    Added functionlity to find the depth of a pit (P, Q, R) as per the instrution.

--- Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
@@ -6,33 +6,43 @@ namespace Inoxico.TechnicalQuestions.Answers
         public static int GetPitDepth(int[] points)
         {
             int N = points.Length;
-            int deeptestPit = -1;
+            int deepestPit = -1;
 
             int start = 0;
 
-            while(start < N - 1)
+            while (start < N - 1)
             {
                 // look for the start of a pit
-                while (start < N - 1 && points[start] <= points[start + 1]){
+                while (start < N - 1 && points[start] <= points[start + 1])
+                {
                     start++;
                 }
 
                 int bottom = start + 1;
                 // look for the bottom  of the pit
-                while(bottom < N - 1 && points[bottom] > points[bottom + 1])
+                while (bottom < N - 1 && points[bottom] > points[bottom + 1])
                 {
                     bottom++;
                 }
 
                 int end = bottom + 1;
                 // look for the  end of the pit
-                while(end < N - 1 && points[end] < points[end + 1])
+                while (end < N - 1 && points[end] < points[end + 1])
                 {
                     end++;
                 }
+
+
+                int depth = Math.Min(points[start] - points[bottom], points[end] - points[bottom]); //The depth of a pit (P, Q, R)
+                if (depth > deepestPit)
+                {
+                    deepestPit = depth;
+                }
+
+                start = end; //update using the last pit
             }
 
-            return 2;
+            return deepestPit;
         }
     }
 }
