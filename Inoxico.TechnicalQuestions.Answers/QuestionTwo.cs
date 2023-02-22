ef2ef661bf51dde99990e884f3e04a554d0e817e
Herold-willcom <heroldm@willcom.co.za>
2023-02-22 22:50:53 +0200

    Added a try catch, incase invalid data gets passed through.

--- Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
@@ -3,46 +3,57 @@ namespace Inoxico.TechnicalQuestions.Answers
 {
     public class QuestionTwo
     {
+        /// <summary>
+        /// Get a pit depth by passing an array
+        /// </summary>
+        /// <param name="points"></param>
+        /// <returns>int</returns>
         public static int GetPitDepth(int[] points)
         {
-            int N = points.Length - 1;
-            int deepestPit = -1;
-
-            int start = 0;
-
-            while (start < N)
+            try
             {
-                // look for the start of a pit
-                while (start < N && points[start] <= points[start + 1])
+                int N = points.Length - 1;
+                int deepestPit = -1;
+
+                int start = 0;
+
+                while (start < N)
                 {
-                    start++;
+                    // look for the start of a pit
+                    while (start < N && points[start] <= points[start + 1])
+                    {
+                        start++;
+                    }
+
+                    int bottom = start + 1;
+                    // look for the bottom  of the pit
+                    while (bottom < N && points[bottom] > points[bottom + 1])
+                    {
+                        bottom++;
+                    }
+
+                    int end = bottom + 1;
+                    // look for the  end of the pit
+                    while (end < N && points[end] < points[end + 1])
+                    {
+                        end++;
+                    }
+
+                    int depth = Math.Min(points[start] - points[bottom], points[end] - points[bottom]); //The depth of a pit (P, Q, R)
+                    if (depth > deepestPit)
+                    {
+                        deepestPit = depth;
+                    }
+
+                    start = end; //update using the last pit
                 }
-
-                int bottom = start + 1;
-                // look for the bottom  of the pit
-                while (bottom < N && points[bottom] > points[bottom + 1])
-                {
-                    bottom++;
-                }
-
-                int end = bottom + 1;
-                // look for the  end of the pit
-                while (end < N && points[end] < points[end + 1])
-                {
-                    end++;
-                }
-
-
-                int depth = Math.Min(points[start] - points[bottom], points[end] - points[bottom]); //The depth of a pit (P, Q, R)
-                if (depth > deepestPit)
-                {
-                    deepestPit = depth;
-                }
-
-                start = end; //update using the last pit
+                return deepestPit;
             }
-
-            return deepestPit;
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return -1;
+            }
         }
     }
 }
