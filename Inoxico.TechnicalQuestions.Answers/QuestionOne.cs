bb1a1d64a5f93b73d8be393d8e5e4ee2baeb2b0e
Herold-willcom <heroldm@willcom.co.za>
2023-02-22 22:00:43 +0200

    Implemented GetLongestSentance(string a) function

--- Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
@@ -3,9 +3,31 @@ namespace Inoxico.TechnicalQuestions.Answers
 {
     public class QuestionOne
     {
+        /// <summary>
+        /// Pass a string that contain sentances, and returns the highest number of words of a specifi sentance
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns>int</returns>
+        /// <exception cref="NotImplementedException"></exception>
         public static int GetLongestSentance(string s)
         {
-            throw new NotImplementedException();
+            string[] sentances = s.Split(new char[] { '.', '?', '!' }); // break down the whole string into sentances based on the specified characters 
+
+            int maxWords = 0;
+            
+            foreach(string sentance in sentances)
+            {
+                string[] currSenWords = sentance.Split(" "); // split the whole sentance into words
+
+                int wordsCount = currSenWords.Where(s => s.Any(Char.IsLetter)).Count(); // confirm that all words included as considered as a letter
+
+                if (wordsCount > maxWords)
+                {
+                    maxWords = wordsCount;
+                }
+            }
+
+            return maxWords;
         }
     }
 }
