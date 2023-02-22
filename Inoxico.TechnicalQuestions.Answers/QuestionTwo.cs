5f809770b839fec3dcfb830b93e6b4924221f80c
Herold-willcom <heroldm@willcom.co.za>
2023-02-22 22:31:05 +0200

    Implementation of question2 initail work. Look for a pit

--- Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
@@ -5,7 +5,34 @@ namespace Inoxico.TechnicalQuestions.Answers
     {
         public static int GetPitDepth(int[] points)
         {
-            throw new NotImplementedException();
+            int N = points.Length;
+            int deeptestPit = -1;
+
+            int start = 0;
+
+            while(start < N - 1)
+            {
+                // look for the start of a pit
+                while (start < N - 1 && points[start] <= points[start + 1]){
+                    start++;
+                }
+
+                int bottom = start + 1;
+                // look for the bottom  of the pit
+                while(bottom < N - 1 && points[bottom] > points[bottom + 1])
+                {
+                    bottom++;
+                }
+
+                int end = bottom + 1;
+                // look for the  end of the pit
+                while(end < N - 1 && points[end] < points[end + 1])
+                {
+                    end++;
+                }
+            }
+
+            return 2;
         }
     }
 }
